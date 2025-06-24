883a23d05fdc025c43325e98007beb97c5872e00
Angelito <tachiangelito@gmail.com>
2025-06-24 11:56:53 -0600

    manejo de roles en el login

--- app/src/main/java/com/example/ingenia/View/LoginActivity.java
+++ app/src/main/java/com/example/ingenia/View/LoginActivity.java
@@ -36,7 +36,6 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         binding.btnEntrar.setOnClickListener(v -> {
             //obtener valores de usuario y password
             String user= binding.txtUsuario.getText().toString();
